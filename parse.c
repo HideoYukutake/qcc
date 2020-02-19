84d629814f9d703e2b0eb76b302148836c1228d5
HideoYukutake <stabiliteanimee@gmail.com>
2020-02-19 22:32:26 +0900

    ステップ11: tokenizerにreturnを判別する処理を追加

--- parse.c
+++ parse.c
@@ -53,6 +53,18 @@ bool startswith(char *p, char *q)
         return memcmp(p, q, strlen(q)) == 0;
 }
 
+int is_alphabet(char c)
+{
+        return ('a'<=c && c<='z') ||
+               ('A'<=c && c<='Z') ||
+               (c == '_');
+}
+
+int is_alnum(char c)
+{
+        return ('0'<=c && c<='9') || is_alphabet(c);
+}
+
 Token *tokenize(char *p)
 {
         Token head;
@@ -96,9 +108,15 @@ Token *tokenize(char *p)
                         continue;
                 }
 
-                if ('a' <= *p && *p <= 'z') {
+                if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+                        cur = new_token(TK_RETURN, cur, p, 6);
+                        p += 6;
+                        continue;
+                }
+
+                if (is_alphabet(*p)) {
                         char *q = p;
-                        while ('a' <= *p && *p <= 'z') {
+                        while (is_alnum(*p)) {
                                 p++;
                         }
                         cur = new_token(TK_IDENT, cur, q, p-q);
@@ -171,6 +189,7 @@ LVar *find_lvar(Token *tok)
  *
  * program    = stmt*
  * stmt       = expr ";"
+ *            | "return" expr ";"
  * expr       = assign
  * assign     = equality ("=" assign)?
  * equality   = relational ("==" relational | "!=" relational)*
--- qcc.h
+++ qcc.h
@@ -9,6 +9,7 @@ typedef enum {
         TK_RESERVED,
         TK_NUM,
         TK_IDENT,
+        TK_RETURN,
         TK_EOF,
 } TokenKind;
 
